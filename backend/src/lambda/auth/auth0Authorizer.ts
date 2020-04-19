341704551fc9d611c4407cfa9b782105280f466b
Yanniel Fandiño <yfandino@aryaly.com>
2020-04-19 13:50:05 +0200

    Add custom autorizerto api

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,15 +9,11 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = process.env.AUTH0_JWTK_URL
 
-export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
+export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
+  
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
@@ -57,11 +53,14 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const jwks = await Axios.get(jwksUrl).then( res => res.data )
+  const key = jwks.keys.find( key => key.kid === jwt.header.kid)
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  if (!key) throw new Error('Unauthrized')
+  
+  const cert = getCert(key.x5c[0])
+  
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -75,3 +74,9 @@ function getToken(authHeader: string): string {
 
   return token
 }
+
+function getCert(key) {
+  key = key.match(/.{1,64}/g).join('\n');
+  key = `-----BEGIN CERTIFICATE-----\n${key}\n-----END CERTIFICATE-----\n`;
+  return key;
+};
